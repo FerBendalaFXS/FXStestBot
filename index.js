f376232423116681ea4f6325c326da49a2c4230e
fbendala <fbendala@fxstreet.com>
2023-11-08 12:07:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// https://github.com/revenkroz/telegram-web-app-bot-example/blob/master/index.html
+
+
+window.onload = () => {
+    const webApp = window.Telegram.WebApp
+
+    webApp.bg_color = 'red'
+    console.log(webApp);
+    console.log(webApp.bg_color);
+}
